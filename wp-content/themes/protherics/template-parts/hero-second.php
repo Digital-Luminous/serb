98f7349a143554d0e989d4c0d37b60ff0b482e7f
Filip Pejovic <f.pejovic@vegait.rs>
2024-11-18 11:08:51 +0100

    Create hero-second banner

---
+++ wp-content/themes/protherics/template-parts/hero-second.php
@@ -0,0 +1,19 @@
+<section class="l-section hero-second s-regular-bottom">
+	<picture class="c-hero-second__bg">
+		<source srcset="http://serb.local/wp-content/themes/protherics/front/static/images/decor-banner-bg.jpg" media="(min-width: 768px)">
+		<img decoding="async" class="c-hero-second__image" src="http://serb.local/wp-content/themes/protherics/front/static/images/decor-banner-bg-mobile.jpg" alt="">
+	</picture>
+	<picture>
+		<source srcset="http://serb.local/wp-content/uploads/2024/11/herobanner-mobile.png" media="(max-width: 768px)">
+		<img class="c-hero-second__img" src="http://serb.local/wp-content/uploads/2024/11/herobanner-image.png" alt="">
+	</picture>
+	<div class="l-inner">
+		<div class="c-hero-second__text-wrap">
+			<div class="c-hero-second__title-wrap">
+				<h1 class="c-hero-second__title">Redefining what’s possible</h1>
+			</div>
+			<h2 class="c-hero-second__subtitle">Redefining what’s possible</h2>
+			<p class="c-hero-second__description">We are passionate in our belief that all patients deserve access to the highest quality medicinal products. Through our deep expertise, we develop and manufacture specialised medicinal products in order to meet the unmet medical needs of our patients.</p>
+		</div>
+	</div>
+</section>
